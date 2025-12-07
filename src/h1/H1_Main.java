611c492a61f2cfec891fcdd947bf8805d8503296
Paul Niederle <paulniederle@MacBook-Air-von-Paul.local>
2025-12-07 10:44:02 +0100

    z

--- src/h1/H1_Main.java
+++ src/h1/H1_Main.java
@@ -2,10 +2,8 @@ package h1;
 
 public class H1_Main {
 	public static void main(String[] args) {
-		Bruch b = new Bruch(15,9);
-		Bruch c = new Bruch(50,30);
-		System.out.println(c.hasSameValueAs(b));
-		c.shorten();
+		Bruch a = new Bruch(15,9);
+		Bruch b = new Bruch(50,30);
 	}
 
 }
