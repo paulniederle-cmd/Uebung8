e76d40c2ec7cfb2127a8699cbc9df6bb4f4bb4bf
Paul Niederle <paulniederle@MacBook-Air-von-Paul.local>
2025-12-11 12:29:15 +0100

    2

--- src/h2/H2_Main.java
+++ src/h2/H2_Main.java
@@ -3,5 +3,12 @@ package h2;
 public class H2_Main {
 	public static void main(String[] args) {
 		SimpleList myList = new SimpleList();
+		myList.append(40);
+		myList.findFirst(40);
+		myList.getFirst();
+		myList.getLast();
+		myList.insertAfter(40,60);
+		myList.delete(60);
+		
 	}
 }
